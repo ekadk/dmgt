9c3b9121230376f07089e6c20d44634a84efdc85
ekadk <ekadharmakusuma3@gmail.com>
2023-06-24 06:15:52 +0700

    fix: add size & priority properties to home image

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="flex-1 lg:flex-[2]">
           {/* Image Start */}
           <div className="w-full h-full object-fill relative">
-            <Image src={"/images/home.png"} alt="home" fill className="object-contain" />
+            <Image src={"/images/home.png"} alt="home" fill className="object-contain" sizes="flex" priority />
           </div>
           {/* Image End */}
         </div>
