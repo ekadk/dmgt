c9b418ec0ba9fa7cd0fc89ad620e721468335d2f
ekadk <ekadharmakusuma3@gmail.com>
2023-06-25 01:14:37 +0700

    fix: tempo page responsive layout

--- app/tempo/page.tsx
+++ app/tempo/page.tsx
@@ -5,10 +5,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="container mx-auto">
-      <div className="flex flex-col lg:flex-row w-full h-screen">
-        <div className="flex-1">
+      <div className="flex flex-col w-full justify-center h-screen lg:flex-row">
+        <div className="w-full flex justify-center items-center lg:flex-[2]">
           {/* Image Start */}
-          <div className="w-full h-full object-fill relative">
+          <div className="w-full aspect-square max-w-md object-fill relative lg:max-w-full">
             <Image
               src={"/images/metronome.png"}
               alt="home"
@@ -20,7 +20,7 @@ export default function Home() {
           </div>
           {/* Image End */}
         </div>
-        <div className="flex-1 flex flex-col items-center lg:justify-center gap-8">
+        <div className="flex lg:flex-1 lg:w-full lg:h-full p-4 justify-center items-start lg:justify-start lg:items-center">
           <div className="flex flex-col justify-center items-center gap-8 w-full max-w-sm lg:border lg:border-black lg:rounded-xl lg:p-8">
             <Title title="Atur Tempo" subtitle="Demi ketukan yang selaras" />
             <div className="w-full max-w-sm px-4 flex items-center justify-center">
